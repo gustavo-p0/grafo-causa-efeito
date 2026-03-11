fc9761b6055753bc703f6945d7dd49e6b0d625d8
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2026-03-11 17:19:09 -0300

    build: github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/grafo-causa-efeito/",
+  build: {
+    outDir: "docs",
+  },
 });
+
 
